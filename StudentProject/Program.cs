5426baad157e13094babbfca4ed178c47baf5c44
brail33na11@gmail.com <brail33na11@gmail.com>
2019-07-24 15:59:08 -0400

    added methods for SetCreditScore

--- StudentProject/Program.cs
+++ StudentProject/Program.cs
@@ -5,19 +5,22 @@ namespace StudentProject {
         static void Main(string[] args)
         {
             Student brailee = new Student();
+            brailee.SetStatus("CURRENT");
             brailee.FirstName = "Brailee";
             brailee.LastName = "Begley";
             brailee.IsPaid = true;
-            brailee.Birthday = new DateTime(1995, 11, 14);
+            brailee.Birthday = new DateTime(1995, 11, 14,16,11,11);
+            
 
-            Student jesse = new Student();
-            jesse.FirstName = "Jesse";
+            Student jesse = new Student("Jesse","Kyle");
+            jesse.SetStatus("PAST");
+            Console.WriteLine($"Student: {jesse.FirstName} {jesse.LastName}");
             jesse.IsPaid = true;
             jesse.Birthday = new DateTime(1990, 1, 1);
 
             Student briana = new Student();
             briana.FirstName = "Briana";
-            briana.LastName = "Presly";
+            briana.LastName = "Presley";
             briana.IsPaid = true;
             briana.Address = "123 Cherry Street";
             briana.City = "Cincinnati";
@@ -27,10 +30,18 @@ namespace StudentProject {
             titus.LastName = "Moore";
             titus.IsPaid = false;
             titus.Phone = "5135555555";
-            titus.Birthday = new DateTime(1999, 04, 27);
+            titus.Birthday = new DateTime(1999, 04, 27)
+            titus.SetCreditScore(9000);
+            titus.SetCreditScore(750);
+            titus.SetCreditScore(350);
+            Console.WriteLine($"Titus's credit score is: {titus.CreditScore}");
 
 
-         }
+            Console.WriteLine($"Brailee's Bootcamp is {brailee.Bootcamp}");
+
+            Console.WriteLine($"Jesse's Bootcamp is {jesse.Bootcamp}");
+
+        }
     }
     class Student {
         public string FirstName { get; set; }
@@ -45,6 +56,38 @@ namespace StudentProject {
         public string Phone { get; set; }
         public bool IsPaid { get; set; } = false;
         public DateTime Birthday { get; set; }
+       public int CreditScore { get; private set; }
+
+
+        public void SetCreditScore(int CreditScore) {
+            if (CreditScore >= 400 && CreditScore <= 900) {
+                this.CreditScore = CreditScore;}
+            else
+            { Console.WriteLine("Credit Score must be between 400 and 900.");
+            }
+        }
+
+        public void SetStatus(string status)
+        {
+            if ((status == "CURRENT")
+                ||(status == "PAST") 
+                || (status=="FUTURE")) {
+                this.Status = status;
+            } else {
+                Console.WriteLine("Status must be CURRENT, PAST, or FUTURE.");
+            }
+           
+        }
+
+        public Student(string FirstName, String LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Bootcamp = "Java";
+        }
+        public Student() {
+            this.Bootcamp = ".Net";
+        }
     }
             }
 
